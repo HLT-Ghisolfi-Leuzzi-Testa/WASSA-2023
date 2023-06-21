ab4a1ac2183fecd276b233df20e9487cab7ca138
Irene Testa <testa.irene1509@gmail.com>
2023-06-21 09:54:53 +0200

    pre-processing cleaning

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,37 @@
 import pandas as pd
 
-DATA_PATH = "datasets/WASSA23_essay_level_with_labels_train.tsv"
-DATA_GOLD_DEV = "datasets/goldstandard_dev.tsv"
+TRAIN_DATA_PATH = "datasets/WASSA23_essay_level_with_labels_train.tsv"
+DEV_DATA_PATH = "datasets/WASSA23_essay_level_dev.tsv"
+DEV_LABELS = "datasets/goldstandard_dev.tsv"
 
-col_names = ["empathy", "distress", "emotion", "personality_conscientiousness", "personality_openes", "personality_extraversion", "personality_agreeableness", "personality_stability", "iri_perspective_taking", "iri_personal_distress", "iri_fantasy", "iri_empathatic_concern"]
+col_names = [ # TODO: check!
+    "empathy",
+    "distress",
+    "emotion",
+    "personality_conscientiousness",
+    "personality_openes",
+    "personality_extraversion",
+    "personality_agreeableness",
+    "personality_stability",
+    "iri_perspective_taking",
+    "iri_personal_distress",
+    "iri_fantasy",
+    "iri_empathatic_concern"
+]
 
-df = pd.read_csv(DATA_PATH, sep='\t')
-df_test = pd.read_csv(DATA_GOLD_DEV, sep='\t', names = col_names)
-
-#df_test.columns(col_names)
+train_df = pd.read_csv(TRAIN_DATA_PATH, sep='\t')
+dev_df = pd.read_csv(DEV_DATA_PATH, sep='\t')
+dev_lbl_df = pd.read_csv(DEV_LABELS, sep='\t', names=col_names)
 
 emotions = set()
-emotion_label = df["emotion"].unique()
-print(emotion_label)
-for label in emotion_label:
-    emotions.update(label.split("/"))
-print(emotions)
-num_classes = len(emotions)
+for perceived_emotion in train_df['emotion'].unique():
+    for emotion in perceived_emotion.split('/'):
+        emotions.add(emotion)
+
 for emotion in emotions:
-    df[emotion] = df["emotion"].str.contains(emotion).astype(int)
+    train_df[emotion] = train_df["emotion"].str.contains(emotion).astype(int)
+    dev_df[emotion] = dev_lbl_df["emotion"].str.contains(emotion).astype(int)
 
-
-# df_train.merge(df_test)  
-#df_merged = df_train.merge(df_test, left_index=True, right_index=True, how='outer')
-df.to_csv("datasets/WASSA23_essay_level_train_preproc.tsv", index = False, sep = '\t') 
+dev_df = dev_df.merge(dev_lbl_df, left_index=True, right_index=True, how='outer')
+train_df.to_csv("datasets/WASSA23_essay_level_train_preproc.tsv", index=False, sep='\t') 
+dev_df.to_csv("datasets/WASSA23_essay_level_dev_preproc.tsv", index=False, sep='\t')
