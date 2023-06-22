d6fad3cca80f9913df5068efe5dbc84735021d9c
Irene Testa <testa.irene1509@gmail.com>
2023-06-22 22:52:33 +0200

    preprocessing and utility functions

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import contractions
+from spellchecker import SpellChecker
 
-TRAIN_DATA_PATH = "datasets/WASSA23_essay_level_with_labels_train.tsv"
-DEV_DATA_PATH = "datasets/WASSA23_essay_level_dev.tsv"
-DEV_LABELS = "datasets/goldstandard_dev.tsv"
-
-col_names = [ # TODO: check!
+TRAIN_DATA_PATH = "https://raw.githubusercontent.com/HLT-Ghisolfi-Leuzzi-Testa/WASSA-2023/main/datasets/WASSA23_essay_level_with_labels_train.tsv"
+DEV_DATA_PATH = "https://raw.githubusercontent.com/HLT-Ghisolfi-Leuzzi-Testa/WASSA-2023/main/datasets/WASSA23_essay_level_dev.tsv"
+DEV_LABELS = "https://raw.githubusercontent.com/HLT-Ghisolfi-Leuzzi-Testa/WASSA-2023/main/datasets/goldstandard_dev.tsv"
+DEV_COL_NAMES = [ # TODO: check!
     "empathy",
     "distress",
     "emotion",
@@ -18,20 +19,102 @@ col_names = [ # TODO: check!
     "iri_fantasy",
     "iri_empathatic_concern"
 ]
+VAL_SIZE = 0.2 # fraction of train data to be used as validation, TODO: 0.1?
+RANDOM_STATE = 42
+
+def one_hot_encode_emotions(df): # TODO: not needed?
+    emotions = set()
+    for perceived_emotion in df['emotion'].unique():
+        for emotion in perceived_emotion.split('/'):
+            emotions.add(emotion)
+    
+    for emotion in emotions:
+        df[emotion] = df["emotion"].str.contains(emotion).astype(int)
+    
+    return df
+
+def clean_text(text): # TODO: prendi parametri in input?
+    '''
+    This function cleans the corpus passed as parameter. 
+
+    :param text: text string
+    :return: cleaned text string
+    '''
+
+    # trim whitespaces
+    text =  " ".join(text.split())
+
+    # expand contractions
+    #text = expand_contractions(text)
+
+    # fix spelling
+    #text = fix_spelling(text)
+
+    # paraphrasing text with low frequecy emotions (?)
+    # https://github.com/adityapatkar/WASSA2023_EMO/blob/main/Wassa_2023_Shared_Task_EMO_Training.ipynb
+
+    return text
+
+def fix_spelling(text): # TODO: many other available: jamspell, symspellpy, textblob
+    '''
+    This function corrects the spelling of the corpus passed as parameter.
+
+    See also:
+    [pyspellchecker](https://pypi.org/project/pyspellchecker/)
+
+    :param text: text string
+    :return: text string corrected
+    '''
+
+    spell_checker = SpellChecker()
+    words = text.split()
+    misspelled_words = spell_checker.unknown(words)
+    corrected_words = []
+    for word in words:
+        if word in misspelled_words:
+            corrected_word = spell_checker.correction(word)
+            if corrected_word:
+              corrected_words.append(corrected_word)
+            else:
+              corrected_words.append(word)
+        else:
+            corrected_words.append(word)
+    return " ".join(corrected_words)
+
+def expand_contractions(text):
+    '''
+    This function expands the contractions of the corpus passed as parameter.
+
+    See also:
+    [contractions](https://pypi.org/project/contractions/)
+
+    :param text: text string
+    :return: text string with expanded contractions
+    '''
+    return ' '.join(contractions.fix(word) for word in text.split())
 
 train_df = pd.read_csv(TRAIN_DATA_PATH, sep='\t')
 dev_df = pd.read_csv(DEV_DATA_PATH, sep='\t')
-dev_lbl_df = pd.read_csv(DEV_LABELS, sep='\t', names=col_names)
+dev_lbl_df = pd.read_csv(DEV_LABELS, sep='\t', names=DEV_COL_NAMES)
 
-emotions = set()
-for perceived_emotion in train_df['emotion'].unique():
-    for emotion in perceived_emotion.split('/'):
-        emotions.add(emotion)
+# TODO: not needed?
+# train_df = one_hot_encode_emotions(train_df)
+# dev_lbl_df = one_hot_encode_emotions(dev_lbl_df)
 
-for emotion in emotions:
-    train_df[emotion] = train_df["emotion"].str.contains(emotion).astype(int)
-    dev_df[emotion] = dev_lbl_df["emotion"].str.contains(emotion).astype(int)
+# text cleaning
+train_df["essay"] = train_df["essay"].apply(clean_text)
+dev_df["essay"] = dev_df["essay"].apply(clean_text)
 
+# splitting train data into train and validation
+# TODO: assicurarsi che train e val abbiano stessa distribuzione di etichette
+train_dataset = train_df.sample(frac=1-VAL_SIZE, random_state=RANDOM_STATE)
+val_df = train_df.drop(train_dataset.index).reset_index(drop=True)
+train_df = train_dataset.reset_index(drop=True)
+
+# merging dev labels with data
 dev_df = dev_df.merge(dev_lbl_df, left_index=True, right_index=True, how='outer')
+
+# saving pre-processed data
 train_df.to_csv("datasets/WASSA23_essay_level_train_preproc.tsv", index=False, sep='\t') 
+val_df.to_csv("datasets/WASSA23_essay_level_val_preproc.tsv", index=False, sep='\t')
 dev_df.to_csv("datasets/WASSA23_essay_level_dev_preproc.tsv", index=False, sep='\t')
---
+++ utils.py
@@ -0,0 +1,110 @@
+import json
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from evaluation import calculatePRF_MLabel
+from torch.utils.data import Dataset
+
+def plot_loss_curve(training_loss, validatin_loss, path):
+    '''
+    This function saves the plot of the training and validation loss curves.
+    
+    :param training_loss: list of training loss values
+    :param validatin_loss: list of validation loss values
+    :param path: path where to save the plot
+    '''
+    
+    sns.set(style='darkgrid')
+    plt.rcParams["figure.figsize"] = (12,6)
+    plt.plot(training_loss, 'b-o', label="Training")
+    plt.plot(validatin_loss, 'r-o', label="Validation")
+    plt.title("Training & Validation Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.xticks(np.arange(1, len(training_loss) + 1, 1))
+    plt.savefig(path)
+    plt.show()
+
+def write_dict_to_json(dict, path):
+    '''
+    This function saves a dictionary to a json file.
+    
+    :param dict: dictionary to save
+    :param path: path where to save the dictionary
+    '''
+    
+    with open(path, 'w') as fp:
+        json.dump(dict, fp)
+
+def write_EMO_prediction(predictions, path):
+    '''
+    This function saves the predictions of the EMO task to a tsv file.
+    
+    :param predictions: list of predictions
+    :param path: path where to save the predictions
+    '''
+    
+    df = pd.DataFrame(predictions)
+    df.to_csv(path, index=False, header=False, sep='\t')
+
+def compute_EMO_metrics(golds, predictions):
+    '''
+    This function computes the metrics for the EMO task.
+    
+    :param golds: list of gold emotions
+    :param predictions: list of predicted emotions
+    '''
+    
+    scores_val = calculatePRF_MLabel(golds, predictions)
+    scores = {
+    'micro_recall': scores_val[0],
+    'micro_precision': scores_val[1],
+    'micro_f': scores_val[2],
+    'macro_recall': scores_val[3],
+    'macro_precision': scores_val[4],
+    'macro_F': scores_val[5],
+    'accuracy': scores_val[6]
+    }
+    return scores
+
+class EMODataset(Dataset):
+    '''
+    This class is used to create a dataset for the EMO task.
+    '''
+
+    def __init__(
+        self,
+        tokenizer,
+        essay,
+        targets,
+        max_len=None # better leaving it to None
+        ):
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.essay = essay
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.essay)
+
+    def __getitem__(self, index):
+        essay = str(self.essay[index])
+        inputs = self.tokenizer.encode_plus(
+            text=essay,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
+
+        return {
+            'input_ids': inputs['input_ids'].flatten(),
+            'attention_mask': inputs['attention_mask'].flatten(),
+            'token_type_ids': inputs["token_type_ids"].flatten(),
+            'targets': torch.FloatTensor(self.targets[index])
+        }
