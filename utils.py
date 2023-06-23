019072e0b7f644749fd92c08c10f96e54b71dc01
Giu <giuliaghisolfi99@gmail.com>
2023-06-23 15:57:11 +0200

    merge

--- utils.py
+++ utils.py
@@ -6,7 +6,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from evaluation import calculatePRF_MLabel
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
+from sklearn.preprocessing import MultiLabelBinarizer
 from torch.utils.data import Dataset
+from transformers import BertForSequenceClassification, EvalPrediction
 
 def plot_loss_curve(training_loss, validatin_loss, path, title):
     '''
@@ -96,6 +99,59 @@ def plot_confusion_matrix(golds, predictions, path, title):
     plt.savefig(path)
     plt.show()
 
+def multi_label_metrics(predictions, labels, threshold=0.5):
+    """https://colab.research.google.com/github/NielsRogge/Transformers-Tutorials/blob/master/BERT/Fine
+    _tuning_BERT_(and_friends)_for_multi_label_text_classification.ipynb#scrollTo=797b2WHJqUgZ"""
+
+    # apply sigmoid on predictions which are of shape (batch_size, num_labels)
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(torch.Tensor(predictions))
+
+    # use threshold to turn them into integer predictions
+    y_pred = np.zeros(probs.shape)
+    y_pred[np.where(probs >= threshold)] = 1
+
+    # compute metrics
+    metrics = compute_EMO_metrics(labels, y_pred)
+
+    # return as dictionary
+    metrics_dict = {
+      'micro_recall': metrics[0],
+      'micro_precision': metrics[1],
+      'micro_fscore': metrics[2],
+      'macro_recall': metrics[3],
+      'macro_precision': metrics[4],
+      'macro_fscore': metrics[5],
+      'accuracy': metrics[6]
+  }
+    return metrics_dict
+
+def compute_metrics(p: EvalPrediction):
+    preds = p.predictions[0] if isinstance(p.predictions, 
+            tuple) else p.predictions
+    result = multi_label_metrics(
+        predictions=preds, 
+        labels=p.label_ids)
+    return result
+
+def save_best_weights(model, MODEL_NAME):
+  torch.save(model.state_dict(), 'checkpoints/model_weights_'+MODEL_NAME+'.pt')
+
+def restore_model(MODEL_NAME, device, num_labels=8): #TODO: MODELLO auto
+  model = BertForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=num_labels)
+  model.load_state_dict(torch.load('checkpoints/model_weights_'+MODEL_NAME+'.pt'))
+  model.to(device)
+  return model
+
+"""def prediction_from_model(model, test_dataloader, mlb, device):
+  with torch.no_grad():
+    input_ids = test_dataloader['input_ids'].to(device)
+    attention_mask = test_dataloader['attention_mask'].to(device)
+    outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+  logits = outputs.logits.detach().cpu().numpy()
+  predictions = encoded2string(logits2encoded(logits), mlb)
+  return predictions"""
+
 class EMODataset(Dataset):
     '''
     This class is used to create a dataset for the EMO task.
@@ -134,3 +190,20 @@ class EMODataset(Dataset):
             'token_type_ids': inputs["token_type_ids"].flatten(),
             'targets': torch.FloatTensor(self.targets[index])
         }
+
+class EmotionLabelEncoder():
+    def __init__(self):
+        self.mlb = MultiLabelBinarizer()
+
+    def fit(self, emotions):
+        emotions = [emotion.split('/') for emotion in emotions]
+        self.mlb.fit(emotions)
+
+    def encode(self, emotions):
+        encoded_emotions = self.mlb.transform(emotions)
+        return encoded_emotions
+
+    def decode(self, encoded_emotions):
+        labels = self.mlb.inverse_transform(np.array(encoded_emotions))
+        emotions = ["/".join(emotion) for emotion in labels]
+        return emotions
