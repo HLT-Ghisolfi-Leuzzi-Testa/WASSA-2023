c705465820eb7fa915b89f46ac6c345ffb06daa4
Giu <giuliaghisolfi99@gmail.com>
2023-07-16 19:07:19 +0200

    new metric train

--- utils.py
+++ utils.py
@@ -454,6 +454,44 @@ def compute_EMO_metrics_trainer(p: EvalPrediction):
     metrics['roc_auc_micro'] = roc_auc_score(y_true=golds, y_score=predictions, average = 'micro')
     return metrics
 
+def compute_EMO_metrics_trainer_label(p: EvalPrediction):
+    '''
+    This function is called by Trainer to compute the metrics for the EMO task.
+
+    :param p: EvalPrediction object
+    :return: dictionary of metrics
+    '''
+
+    predictions = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+    golds = p.label_ids
+    golds = np.where(golds >= 0.5, 1, 0)
+    golds = golds.astype(int)
+
+    # use a threshold to turn prediction into 0/1 values
+    bin_predictions = np.where(predictions >= 0.5, 1, 0)
+    # if no emotion is predicted, set the one with highest activation
+    for i, bin_pred in enumerate(bin_predictions):
+        if np.all(bin_pred==0):
+            bin_predictions[i][np.argmax(predictions[i])] = 1
+    predictions = bin_predictions
+
+    # compute metrics
+    metrics = {}
+
+    prf_macro = precision_recall_fscore_support(y_true=golds, y_pred=predictions, average='macro')
+    prf_micro = precision_recall_fscore_support(y_true=golds, y_pred=predictions, average='micro')
+
+    metrics['macro_f1'] = prf_macro[2]
+    metrics['micro_f1'] = prf_micro[2]
+    metrics['micro_jaccard'] = jaccard_score(y_true=golds, y_pred=predictions, average='micro')
+    metrics['macro_precision'] = prf_macro[0]
+    metrics['macro_recall'] = prf_macro[1]
+    metrics['micro_precision'] = prf_micro[0]
+    metrics['micro_recall'] = prf_micro[1]
+    metrics['sklearn_accuracy'] = accuracy_score(y_true=golds, y_pred=predictions)
+    metrics['roc_auc_micro'] = roc_auc_score(y_true=golds, y_score=predictions, average = 'micro')
+    return metrics
+
 def compute_EMO_metrics_trainer_neutral(p: EvalPrediction):
     '''
     This function is called by Trainer to compute the metrics for the EMO task.
